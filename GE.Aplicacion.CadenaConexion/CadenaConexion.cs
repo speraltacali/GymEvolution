57ad21d92ea398bdf7d025211f094f618aa7a677
jose sabeckis flores <josesabeckis_pj@hotmail.com.ar>
2019-09-29 00:52:12 -0300

    dasda

--- GE.Aplicacion.CadenaConexion/CadenaConexion.cs
+++ GE.Aplicacion.CadenaConexion/CadenaConexion.cs
@@ -4,16 +4,19 @@ namespace GE.Aplicacion.CadenaConexion
 {
     public class CadenaConexion
     {
-        public const string DataBase = "GymEvolution";
+        public const string DataBase = "GymVersion6";
 
-        public const string Server = @"DESKTOP-NK0OJF1\SQLSANTI";
-
+        public const string Server = @"DESKTOP-066FGIK\SQLEXPRESS";
+        /*
         public const string User = "sa";
 
-        public const string Password = "Santi42175";
+        public const string Password = "Santi42175";*/
 
         public static string AccesoCadenaConexion => $"Data Source={Server};" +
                                                      $"Initial Catalog={DataBase};" +
                                                      $"Integrated Security=true";
+
+        /*    $"User Id={User};" +
+            $"Password={Password};";*/
     }
 }
