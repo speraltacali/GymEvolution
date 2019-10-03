9fc3e91cc24f3b62561ff847425e37d728df90aa
Santiago Agustin Peralta Cali <santiagoperaltacali@hotmail.com>
2019-10-03 16:16:27 -0300

    Validacion Login

---
+++ GE.Infraestructura.Context/Migrations/20191003183226_SegundoDemo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GE.Infraestructura.Context.Migrations
+{
+    public partial class SegundoDemo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
