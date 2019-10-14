15acbb829efafb589d8ffbb5f08341aced10557c
Fernando Baunaly <stiilecs@outlook.com>
2019-10-14 19:43:29 -0300

    dada

---
+++ GE.Presentacion.GymEvolution/Controllers/CajaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GE.Presentacion.GymEvolution.Controllers
+{
+    public class CajaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            //hola fer
+        }
+    }
+}
