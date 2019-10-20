d6312a8529d33d4f41e54bfcb2d87bb1a315dea7
jose sabeckis flores <josesabeckis_pj@hotmail.com.ar>
2019-10-20 01:28:56 -0300

    dada

---
+++ GE.Infraestructura.Context/Migrations/20191019193822_0001adna.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GE.Infraestructura.Context.Migrations
+{
+    public partial class _0001adna : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
