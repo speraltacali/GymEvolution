d0ddde524c0ffb8d6ae0bedc67758c17f8915408
Santiago Agustin Peralta Cali <santiagoperaltacali@hotmail.com>
2019-11-14 19:12:39 -0300

    Terminando Rutina

---
+++ GE.IServicio/Rutina/IRutinaServicio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GE.IServicio.Rutina.DTO;
+
+namespace GE.IServicio.Rutina
+{
+    public interface IRutinaServicio
+    {
+        RutinaDto Agregar(RutinaDto dto);
+
+        RutinaDto Modificar(RutinaDto dto);
+
+        void Eliminar(long id);
+
+        IEnumerable<RutinaDto> ObtenerTodo();
+
+        RutinaDto ObtenerPorId(long id);
+
+        IEnumerable<RutinaDto> ObtenerPorFiltro(string cadena);
+    }
+}
---
+++ GE.Infraestructura.Repositorio/Rutina/RutinaRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GE.Dominio.Repositorio.Rutina;
+using GE.Repositorio;
+
+namespace GE.Infraestructura.Repositorio.Rutina
+{
+    public class RutinaRepositorio : Repositorio<Dominio.Entity.Entidades.Rutina> , IRutinaRepositorio 
+    {
+    }
+}
