4178ae4591005d6690f5d4a10c6ac40425f22c60
jose sabeckis flores <josesabeckis_pj@hotmail.com.ar>
2019-11-16 16:54:12 -0300

    correcciones de vista parte 1/4

--- GE.Aplicacion.CadenaConexion/CadenaConexion.cs
+++ GE.Aplicacion.CadenaConexion/CadenaConexion.cs
@@ -6,22 +6,20 @@ namespace GE.Aplicacion.CadenaConexion
     {
 
 
-        public const string DataBase = "GymEvolutionDB2019";
+        public const string DataBase = "XXX7";
 
+        public const string Server = @"DESKTOP-066FGIK\SQLEXPRESS";
+        /*
+        public const string User = "sa";
 
-        public const string Server = @"tcp:gymevolutiondbserver.database.windows.net";
-
-
-        public const string User = "softgym@gymevolutiondbserver";
-
-        public const string Password = " GYM2019soft";
-
+        public const string Password = "Santi42175";*/
 
         public static string AccesoCadenaConexion => $"Data Source={Server};" +
                                                      $"Initial Catalog={DataBase};" +
-                                                     //$"Integrated Security=true"; +
-                                                     $"User Id={User};" +
-                                                     $"Password={Password};";
+                                                     $"Integrated Security=true";
+
+        /*    $"User Id={User};" +
+            $"Password={Password};";*/
     }
 
     /*
